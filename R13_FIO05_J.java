ff0ffcdd8cbc8f75787cd8b56d8bc6d069009f29
carsonreilly <146482394+carsonreilly@users.noreply.github.com>
2023-10-06 15:43:13 -0400

    Create R13_FIO05_J.java
    Created Rule 13 code with vulnerabilities.

---
+++ R13_FIO05_J.java
@@ -0,0 +1,11 @@
+final class Wrap{
+   private char[] dataArray;
+
+   public Wrap(){
+      dataArray = new char[10];
+      // Initialize
+   }
+   public CharBuffer getBufferCopy(){
+      return charBuffer.wrap(dataArray);
+   }
+}
